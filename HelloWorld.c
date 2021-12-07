634d681fd3fdc260c6082ab7c5e0f794e686bb14
daseulll <nldaseul@gmail.com>
2021-12-08 07:16:46 +0900

    double과 float의 표현 오차 확인

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,18 @@
 
 int main(void)
 {
-    printf("Hello World!\n");
+    // 123.0;  // double;
+    // 123.0F; // float
+
+    // printf("%f\n", 123.45F);
+    // printf("%f\n", 123.45);
+
+    printf("%f\n", 2147483648.0);
+    printf("%f\n", 2147483648.0 - 20);
+    printf("%f\n", 2147483648.0 - 40);
+    printf("%f\n", 2147483648.0 - 60);
+    printf("%f\n", 2147483648.0 - 64);
+    printf("%f\n", 2147483648.0 - 70);
+    printf("%f\n", 2147483648.0 - 80);
     return 0;
 }
